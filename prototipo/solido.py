3be000a4651fed1fa490972f94db926d834a6b1a
TiagoGama <tiago.gama@grad.ufsc.br>
2021-07-31 17:51:04 -0300

    falta método

--- prototipo/solido.py
+++ prototipo/solido.py
@@ -1,1 +1,8 @@
 from obstaculo import IObstaculo
+
+class Solido(IObstaculo):
+  def __init__(self, width, height, cor, margem, velocidade):
+      super().__init__(width, height, cor, margem, velocidade)
+
+  def mover(self):
+    pass
