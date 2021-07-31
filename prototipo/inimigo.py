35b976685644387b7a7048983b665e6fb0f33a5c
TiagoGama <tiago.gama@grad.ufsc.br>
2021-07-31 17:53:53 -0300

    inimigo, falta métodos

--- prototipo/inimigo.py
+++ prototipo/inimigo.py
@@ -1,1 +1,12 @@
 from obstaculo import IObstaculo
+
+class Inimigo(IObstaculo):
+  def __init__(self, width, height, cor, margem, velocidade, poderAttack):
+      super().__init__(width, height, cor, margem, velocidade)
+      self.__poderAttack = poderAttack
+    
+  def atacar(self):
+    pass
+
+  def mover(self):
+    pass
