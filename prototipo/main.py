a0483e9e1c166044fd5d74ce986f07e25782d11d
TiagoGama <tiago.gama@grad.ufsc.br>
2021-07-31 19:39:43 -0300

    função de inicio e tela implementada

--- prototipo/main.py
+++ prototipo/main.py
@@ -1,12 +1,13 @@
 import pygame
+from Canvas import *
 import os
 import random
 
 class Engine:
-    def __init__(self, Player, Canvas) -> None:
+    def __init__(self, Player, canvas:Canvas) -> None:
         self.__player = Player
-        self.__Tela = Canvas
-        self.__isPlaying = False
+        self.__Tela = canvas
+        self.__isPlaying = True
         self.__isPaused = False
     
     def gerar_obstaculos(self):
@@ -14,3 +15,20 @@ class Engine:
 
     def gerar_poderes(self):
         pass
+    
+    def gameLoop(self):
+
+        """ seta configuração de tela no objeto Tela com width/height da classe canvas """
+        self.__Tela.iniciarTela
+        
+        """ Configuração básica pygame """
+        while self.__isPlaying:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.__isPlaying = False
+        pygame.quit()
+        
+        
+""" Iniciando o Game """
+engine = Engine('Player',Canvas(500,500))
+engine.gameLoop()
