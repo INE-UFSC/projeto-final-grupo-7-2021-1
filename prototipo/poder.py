7e5c421a0c6b29bdef6c7a3e080eff31a8e4a754
AugustoC3sar <c.augusto0302@gmail.com>
2021-08-16 21:17:08 -0300

    Limpando tudo e começando de novo

--- prototipo/poder.py
+++
@@ -1,8 +0,0 @@
-class Poder:
-    def __init__(self,tipo,duracao):
-        self.__tipo = tipo
-        self.__duracao = duracao
-    """ tipo = invisibilidade / explosão, slow """
-    def executar_acao(self):
-        pass
-
--- prototipo/solido.py
+++
@@ -1,8 +0,0 @@
-from obstaculo import IObstaculo
-
-class Solido(IObstaculo):
-  def __init__(self, width, height, cor, margem, velocidade):
-      super().__init__(width, height, cor, margem, velocidade)
-
-  def mover(self):
-    pass
