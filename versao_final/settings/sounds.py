64c600fbdf697467be1dd8cad71be2d03e92b84b
TiagoGama <tiago.gama@grad.ufsc.br>
2021-09-16 16:02:07 -0300

    sound config/class

---
+++ versao_final/settings/sounds.py
@@ -0,0 +1,25 @@
+from pygame.mixer import Sound
+import os
+
+PATH = os.path.join(os.getcwd(), 'assets', 'sounds_assets')
+
+class GameMusic(Sound):
+  def __init__(self):
+    super().__init__(os.path.join(PATH,'music.mp3'),)
+
+class ButtonSfxSound(Sound):
+  def __init__(self):
+    super().__init__(os.path.join(PATH,'button_sfx.mp3'),)
+
+class GameOverSound(Sound):
+  def __init__(self):
+    super().__init__(os.path.join(PATH,'game_over.mp3'),)
+
+class JumpSfxSound(Sound):
+  def __init__(self):
+    super().__init__(os.path.join(PATH,'jump_sfx.mp3'),)
+
+class MenuMusic(Sound):
+  def __init__(self):
+    super().__init__(os.path.join(PATH,'menu.mp3'),)
+
