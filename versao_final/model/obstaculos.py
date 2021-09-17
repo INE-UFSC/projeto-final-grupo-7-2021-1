bc4fd0b816fea809962b6bf2df1fbf10e1c94def
TiagoGama <tiago.gama@grad.ufsc.br>
2021-09-17 10:40:58 -0300

    bug

--- versao_final/model/obstaculos.py
+++ versao_final/model/obstaculos.py
@@ -10,7 +10,7 @@ class Obstaculo( Sprite, ABC):
     def __init__(self, x, y, margem, filename, scale):
         super().__init__()
         self.__margem = margem
-        self.image = pygame.transform.scale(pygame.image.load(os.path.join(GAME_SETTINGS.obstacle_path, filename)), scale)
+        self.image = pygame.transform.scale(pygame.image.load(os.path.join(os.getcwd(),'assets', 'obstacles', GAME_SETTINGS.obstacle_path, filename)), scale)
         
         self.rect = self.image.get_rect()
         self.rect.x = x
